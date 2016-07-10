d3b8ace6c3274af5305038f45c2d45af6f3d23e0
FangChu <imfangchu@gmail.com>
2016-07-11 00:16:22 +0800

    updated index

--- js/main-self.js
+++ js/main-self.js
@@ -1,14 +1,15 @@
 $(document).on('ready', function() {
   $('.variable-width').slick({
-  breakpoint: 1024,  
+  breakpoint: 1024,
   dots: true,
   infinite: true,
   speed: 300,
   slidesToShow: 1,
   centerMode: true,
   variableWidth: true,
-  autoplay: true,
+  autoplay: false,
+  });
 
-});
 
+  $('.single-item').slick();
 });
