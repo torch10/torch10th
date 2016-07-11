7265b0f72fb079a640854848389b3e19a14127b8
chufang <fangfangchu@rakuya.com.tw>
2016-07-11 22:20:30 +0800

    update vedio plugin

--- js/main-self.js
+++ js/main-self.js
@@ -1,24 +1,8 @@
-$(document).on('ready', function() {
-  $(".goto").click(function() {
-    $('body,html').animate({
+$( document ).ready(function() {
+  $(".goto").click(function(e) {
+    e.preventdefault();
+    $(".wrapper").animate({
         scrollTop: $("#here").offset().top
       }, 1000);
   });
-
-});
-
-$(document).on('ready', function() {
-  $('.variable-width').slick({
-  breakpoint: 1024,
-  dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  centerMode: true,
-  variableWidth: true,
-  autoplay: false,
-  });
-
-  $('.single-item').slick();
-
 });
